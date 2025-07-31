42f6b967c664e11fe8720d03d1c3a5a0f437a17b
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-07-31 19:43:58 +0300

    Add player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VContainer;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    [Inject] private PlayerMovement _movement;
     void Update()
     {
 
+    }
+    void FixedUpdate()
+    {
+        _movement.HandleMovement();
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float decelerationSpeed;
+    [SerializeField] private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
+    public void HandleMovement()
     {
-        
+        Vector2 moveAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (moveAxis == Vector2.zero)
+        {
+            rb.velocity = Vector3.MoveTowards(rb.velocity, Vector2.zero, decelerationSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.AddForce(moveAxis.normalized * maxSpeed, ForceMode2D.Force);
+
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+        }
     }
 }
