bc9f6fc556db2372ebea12d2e66888cd7c12f8d6
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-08-01 17:28:42 +0300

    Add IKillable interface

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using VContainer;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IKillable
 {
     [Inject] private PlayerMovement _movement;
     [Inject] private PlayerAim _aim;
     [Inject] private PlayerWeaponController _weapon;
+    private bool isAlive = true;
     void Update()
     {
+        if (!isAlive) return;
+
         _weapon.HandleWeaponControl();
     }
     void FixedUpdate()
     {
+        if (!isAlive) return;
+
         _movement.HandleMovement();
         _aim.HandleAim();
+    }
+
+    public void Kill()
+    {
+        isAlive = false;
     }
 }
